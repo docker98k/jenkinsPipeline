a6157e74465f14ed35da3fbf26cdb14b81797f8a
lind.zhang <bfyxzls@sina.com>
2018-07-10 12:17:24 +0800

    解决脚本问题

---
+++ vars/servicePipeline.groovy
@@ -0,0 +1,31 @@
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipeline {
+        agent { label 'build-server' }
+       
+        stages {
+            stage('Initialization') {
+                steps {
+                    script {
+                       echo "项目初始化"
+                    }
+                }
+            }
+            stage('CI') {
+                steps{
+                    script{
+                        gitCheckout{
+                            repoUrl = config.repoUrl
+                            credentialsId = config.credentialsId
+                            branches = config.branches
+                            commit = config.commit
+                        }
+                    }
+                }
+            }
+        }
+    }
