7c1efe674a7e6935b8cb630275ae133d52e05829
lind.zhang <bfyxzls@sina.com>
2018-07-10 17:09:43 +0800

    [feat] #316 添加两个字段以适应天津那边的实体

--- vars/dockerImageDeploy.groovy
+++ vars/dockerImageDeploy.groovy
@@ -1,25 +1,23 @@
 def call(body) {
-    def config = [
-        registry: "registry.docker.cn"
-    ]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+    def config = [registry: "registry.docker.cn"] 
+    body.resolveStrategy = Closure.DELEGATE_FIRST 
+    body.delegate = config 
+    body() 
     stage('deploy') {
-    script{
-    echo "Deploy Image"
-    if (config.imageName == null ||config.imageName == 'null' || config.imageName == '') {
-    	throw new Exception("image name is missing!")
-    } 
-    if (config.tagId == null ||config.tagId == 'null' || config.tagId == '') {
-    	throw new Exception("tag is missing!")
-    } 
-    sh """
+        script {
+            echo "Deploy Image"
+            if (config.imageName == null || config.imageName == 'null' || config.imageName == '') {
+                throw new Exception("image name is missing!")
+            }
+            if (config.tagId == null || config.tagId == 'null' || config.tagId == '') {
+                throw new Exception("tag is missing!")
+            }
+            sh """
         docker tag ${config.imageName}:${config.tagId} ${config.registry}/${config.imageName}:${config.tagId}
         docker tag ${config.imageName}:${config.tagId} ${config.registry}/${config.imageName}:latest
         docker push ${config.registry}/${config.imageName}:${config.tagId}
         docker push ${config.registry}/${config.imageName}:latest
        """
-}
- }
+        }
+    }
 }
--- vars/dotnetcoreBuild.groovy
+++ vars/dotnetcoreBuild.groovy
@@ -1,13 +1,13 @@
 def call(body) {
-    def config = [ : ]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+    def config = [: ] 
+    body.resolveStrategy = Closure.DELEGATE_FIRST 
+    body.delegate = config 
+    body() 
     stage('building') {
-                echo 'Building DotNet Project'
-                script {
-                    sh returnStdout: true, script: "docker run -v $workspace/:/src/  --workdir=${config.workspace} --user root --tty --rm microsoft/dotnet:2.0.0-sdk dotnet build"
-                }   
-            }
+        echo 'Building DotNet Project'script {
+            sh returnStdout: true,
+            script: "docker run -v $workspace/:/src/  --workdir=${config.workspace} --user root --tty --rm microsoft/dotnet:2.0.0-sdk dotnet build"
+        }
+    }
 
 }
