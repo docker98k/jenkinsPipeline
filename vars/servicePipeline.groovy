dd428f8e7b215f872deb582df9b84438ef4926a1
lind.zhang <bfyxzls@sina.com>
2018-07-10 18:36:57 +0800

    添加docker服务

--- vars/servicePipeline.groovy
+++ vars/servicePipeline.groovy
@@ -14,23 +14,33 @@ def call(Map pipelineParams) {
             stage('CI') {
                 steps{
                     script{
+                        // 迁出源代码
                         gitCheckout{
                             repoUrl = pipelineParams.repoUrl
                             credentialsId = pipelineParams.credentialsId
                             branches = pipelineParams.branches
                             commit = pipelineParams.commit
                         }
+                        // 编译和发布项目
                         dotnetcoreBuild{
                             workspace = pipelineParams.workspace
                         }
+                        // 打包成镜像
                         dockerImageBuild{
                             imageName=pipelineParams.imageName
                             tagId=pipelineParams.tagId
                             context=pipelineParams.context
                         }
+                        // 将镜像推到远程仓库
                     }
                 }
             }
+            stage('Service') {
+                script{
+                    // 启动服务
+                }
+            }
+
         }
     }
 }
