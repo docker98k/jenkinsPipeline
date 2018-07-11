14f6f6770e47c046222e828c932bddd254b74c1b
lind.zhang <bfyxzls@sina.com>
2018-07-11 16:29:52 +0800

    add file

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -1,17 +1,17 @@
 //打包镜像
 def call(body) {
-    def config = [ : ]
+    def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
     if (config.imageName == null ||config.imageName == 'null' || config.imageName == '') {
-    	throw new Exception("image name is missing!")
+    	throw new Exception("image name is missing! imageName="+config.imageName)
     } 
     if (config.tagId == null ||config.tagId == 'null' || config.tagId == '') {
-    	throw new Exception("tag is missing!")
+    	throw new Exception("tag is missing! imageName="+config.imageName)
     } 
     if (config.context == null ||config.context == 'null' || config.context == '') {
-    	throw new Exception("context is missing!")
+    	throw new Exception("context is missing! imageName="+config.imageName)
     } 
 
     sh "docker build --pull -t ${config.imageName}:${config.tagId} ${config.context}"
